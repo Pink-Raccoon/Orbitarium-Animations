840c8a366d4bb8a6cbc720b212d188829d3a0151
Nicolas Schaller <nicolas_schaller@hotmail.ch@users.noreply.github.com>
2020-08-04 15:15:06 +0200

    Set correct zoom level for 2160x2160 Pixels

--- animation/corona_spread/js/map.js
+++ animation/corona_spread/js/map.js
@@ -1,10 +1,13 @@
 var map;
 
 function initMap() {
-    //zoom level must be at 3.485 for a render screen of 2000x2000 pixels (web mercator is a square!)
+    //zoom level must be at 3.0768155970508308922315362613328397021 for a render screen of 2160x2160 pixels (web mercator is a square!)
+    //at zoom level 0, the world map is projected using a single tile of 256x256 pixels. Manycam renders 4k @ 3840x2160 pixels.
+    //we need the mercator projection of the world only once on a resolution of 2160x2160 pixels. formula:
+    //2160 = 256 * 2^zoom
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 0, lng: 0 },
-        zoom: 3.485
+        zoom: 3.0768155970508308922315362613328397021
     });
     console.log(getAnimationInit());
     //var polygon = drawPolyLine("asd");
